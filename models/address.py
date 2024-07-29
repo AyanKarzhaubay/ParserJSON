78834efdff1adbc40600d3829902639ff6e533c0
Ayan Karzhaubay <ayan.karzhaubay@gmail.com>
2024-07-29 18:17:56 +0500

    deleting models

--- models/address.py
+++
@@ -1,9 +0,0 @@
-class Address:
-    def __init__(self, street: str, city: str, zipcode: str):
-        self.street = street
-        self.city = city
-        self.zipcode = zipcode
-
-    def __repr__(self):
-        return f"Address(street='{self.street}', city='{self.city}', zipcode='{self.zipcode}')"
-
--- models/course.py
+++
@@ -1,8 +0,0 @@
-class Course:
-    def __init__(self, id: int, course_name: str):
-        self.id = id
-        self.course_name = course_name
-
-    def __repr__(self):
-        return f"Course(id={self.id}, course_name='{self.course_name}')"
-
